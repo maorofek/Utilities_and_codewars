09b8482bb7898cc6d95353f99c4839fba124e194
Maor <zeusofek@gmail.com>
2022-04-08 15:18:47 +0300

    HumanReadableTime added func

--- codewars/HumanReadableTime.java
+++ codewars/HumanReadableTime.java
@@ -3,13 +3,13 @@ import java.text.DecimalFormat;
 public class HumanReadableTime {
 
     public static void main(String[] args) {
-//        System.out.println(makeReadable(0));
-//        System.out.println(makeReadable(5));
-//        System.out.println(makeReadable(60));
-//        System.out.println(makeReadable(86399));
-//        System.out.println(makeReadable(3600));
+        System.out.println(makeReadable(0));
+        System.out.println(makeReadable(5));
+        System.out.println(makeReadable(60));
+        System.out.println(makeReadable(86399));
+        System.out.println(makeReadable(3600));
         System.out.println(makeReadable(352881));
-//        System.out.println(makeReadable(359999));
+        System.out.println(makeReadable(359999));
 
     }
 
@@ -28,5 +28,9 @@ public class HumanReadableTime {
         String bFormatString = formatter.format(min);
         String cFormatString = formatter.format(hours);
         return cFormatString + ":" + bFormatString + ":" + aFormatString;
+    }
+
+    public static String makeReadable2(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
     }
 }
