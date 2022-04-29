fbba1ea0c761c582f9845c3473e0320d4737eef9
Maor <zeusofek@gmail.com>
2022-04-29 23:27:06 +0300

    BreakCamelCase done

--- codewars/BreakCamelCase.java
+++ codewars/BreakCamelCase.java
@@ -1,18 +1,27 @@
 public class BreakCamelCase {
     public static void main(String[] args) {
-
-
+        System.out.println(camelCase2("camelCasing"));
+        System.out.println(camelCase2("camelCasingTest"));
+        System.out.println(camelCase2("camelcasingtest"));
+        System.out.println(camelCase2("itIsPossible"));
     }
 
     //TODO
     public static String camelCase(String input) {
-        char c;
+        StringBuilder str = new StringBuilder();
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) <= 90) {
-
+            str.append(input.charAt(i));
+            if (i + 1 < input.length()) {
+                if (Character.isUpperCase(input.charAt(i + 1))) {
+                    str.append(" ");
+                }
             }
-
         }
-        return null;
+        return str.toString();
+    }
+
+    //Another sol
+    public static String camelCase2(String input) {
+        return input.replaceAll("([A-Z])", " $1");
     }
 }
