4f09cb43f852f3e234fa6aba7e0f097069f5d931
Maor <zeusofek@gmail.com>
2022-05-02 15:30:17 +0300

    CountOfPositivesSumOfNegatives

--- codewars/CountOfPositivesSumOfNegatives.java
+++ codewars/CountOfPositivesSumOfNegatives.java
@@ -6,16 +6,17 @@ public class CountOfPositivesSumOfNegatives {
         System.out.println(Arrays.toString(countPositivesSumNegatives(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15})));
     }
 
-    //TODO
     public static int[] countPositivesSumNegatives(int[] input) {
-//        if (input.length == 0 || input[0] == 0 || input[1] == 0) {
-//            return new int[0];
-//        }
+        if (input == null || input.length == 0) {
+            return new int[0];
+        }
+        System.out.println(Arrays.toString(input));
+
         int[] result = new int[2];
         Arrays.stream(input).forEach(x -> {
             if (x > 0) {
                 result[0]++;
-            } else {
+            } else if (x < 0) {
                 result[1] += x;
             }
         });
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<String> c = new ArrayList<>();
 
     }
 }
