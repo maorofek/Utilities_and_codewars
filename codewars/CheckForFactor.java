4745c1c6de8f9e18e23e38c268e5cf2626460d39
Maor <zeusofek@gmail.com>
2022-06-29 09:55:21 +0300

    CheckForFactor

---
+++ codewars/CheckForFactor.java
@@ -0,0 +1,11 @@
+public class CheckForFactor {
+    public static void main(String[] args) {
+        System.out.println(checkForFactor(10, 2));
+        System.out.println(checkForFactor(63, 7));
+        System.out.println(checkForFactor(2450, 5));
+    }
+
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
