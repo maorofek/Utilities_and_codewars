1d68aa3c94196cc3e6bc1aa023bf49bcbe3c80d5
Maor.O <zeusofek@gmail.com>
2022-07-05 07:27:57 +0300

    LinearEquationSolver start

---
+++ cw/LinearEquationSolver.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class LinearEquationSolver {
+    public static void main(String[] args) {
+
+    }
+    //TODO
+    public Map<String, Double> solve(String... equations) {
+
+        // Your code here!
+        return null;
+    }
+}
