782e902fb0db8e712659a02a9b5cfc58c39945cd
Maor <zeusofek@gmail.com>
2022-07-07 14:32:24 +0300

    BreakingChocolateProblem

---
+++ cw/BreakingChocolateProblem.java
@@ -0,0 +1,9 @@
+public class BreakingChocolateProblem {
+    public static void main(String[] args) {
+
+    }
+
+    public static int breakChocolate(int n, int m) {
+        return Math.max(n * m - 1, 0);
+    }
+}
