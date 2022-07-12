296cbff2972470b62f1c786d6a6a56cb4716a8dd
Maor.O <zeusofek@gmail.com>
2022-07-12 20:02:18 +0300

    WhoWonTheElection in progress

---
+++ cw/WhoWonTheElection.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class WhoWonTheElection {
+
+    public static void main(String[] args) {
+        System.out.println(getWinner(Arrays.asList("A", "A", "A", "B", "B", "B", "A")));
+    }
+
+    public static String getWinner(final List<String> listOfBallots) {
+
+
+    }
+
+}
