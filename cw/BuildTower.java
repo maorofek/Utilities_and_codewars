7abc27f106bb197f35cd23ae4247e28d08cd4aa1
Maor.O <zeusofek@gmail.com>
2022-08-07 11:14:15 +0300

    BuildTower done

--- cw/BuildTower.java
+++ cw/BuildTower.java
@@ -28,3 +28,13 @@ public class BuildTower {
         return tower;
     }
 }
+  /*
+  public static String[] TowerBuilder(int n) {
+    String t[] = new String[n], e;
+
+    for (int i = 0; i < n; i++)
+        t[i] = (e = " ".repeat(n-i-1)) + "*".repeat(i+i+1) + e;
+
+    return t;
+}
+    */
